ce40d821499663924bab5f9833730adb458e4b40
stevenacz <scoailazaa@gmail.com>
2020-11-02 11:17:47 -0500

    Prueba 2

--- 02bmi_calculator/bmi_calculator.rb
+++ 02bmi_calculator/bmi_calculator.rb
@@ -5,3 +5,16 @@
 # Calculate the BMI and print the message with the result
 
 # If the result is less than 18.5 print "You are underweight, add more potato to the broth."
+
+puts "Weight (kg)"
+weight = gets
+puts "Height (m)"
+height = gets
+
+BMI = ((weight.to_f / height.to_f**2) * 100).round / 100.0
+
+puts BMI
+
+if BMI < 18.5
+  puts "You are underweight, add more potato to the broth."
+end
