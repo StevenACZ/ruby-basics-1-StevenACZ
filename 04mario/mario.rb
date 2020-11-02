65a19b660fe20746336e35da81ae315898833c4a
stevenacz <scoailazaa@gmail.com>
2020-11-02 18:17:22 -0500

    Problema 04

--- 04mario/mario.rb
+++ 04mario/mario.rb
@@ -1,3 +1,35 @@
 # This and the next exercise won't have useful comments to guide you.
 # Start by thinking in pseudocode which steps you need to do to solve the proble.
 # Then, start writing beatuful ruby code! Go for it!
+height = 0
+loop do
+  print("Pyramid height: ")
+  height = gets.to_i
+
+  height.positive? && height < 8 && break
+end
+
+y = height - 1
+x = 1
+while x <= height
+  while y >= 0
+    print(" " * y)
+    print("#" * x)
+    print("\n")
+    x += 1
+    y -= 1
+  end
+  x += 1
+end
+
+
+# y = height - 1
+# for x in 1..height
+#   while y >= 0
+#     print(" " * y)
+#     print("#" * x)
+#     print("\n")
+#     x += 1
+#     y -= 1
+#   end
+# end
