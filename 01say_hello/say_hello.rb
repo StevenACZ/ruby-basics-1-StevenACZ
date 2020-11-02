11186206ee06e8ef3b106890290ed0a7eb10816f
stevenacz <scoailazaa@gmail.com>
2020-11-02 10:38:56 -0500

    Prueba 2

--- 01say_hello/say_hello.rb
+++ 01say_hello/say_hello.rb
@@ -2,3 +2,10 @@
 
 # If the user enters some text like "Monica", print "Hello Monica! Welcome to the Ruby module."
 # If the user enters nothing, print "No one to say hello to. :cry:"
+name = gets
+
+if name.strip.length != 0
+  puts "Hello #{name}! Welcome to the Ruby module."
+else
+  puts "No one to say hello to. :cry:"
+end
