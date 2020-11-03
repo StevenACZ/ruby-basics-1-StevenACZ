bbf1bb9a88fbb2bd80b345b4cc47bdfcef085d05
stevenacz <scoailazaa@gmail.com>
2020-11-02 20:15:59 -0500

    Problema 05

--- 05cash/cash.rb
+++ 05cash/cash.rb
@@ -1,1 +1,37 @@
 # This exercise won't have useful comments to guide you. Go for it!
+# 1 Sol, 50 centimos, 20 centimos and 10 centimos
+
+cash = 0
+loop do
+  print("Change owed: ")
+  cash = gets.to_f
+
+  cash.positive? && break
+end
+
+cents = cash.to_s.split(".").last.to_i
+cash.to_i.zero? && cents = cash.to_s.split(".").last.to_i.round(-1)
+cents.to_s.length == 1 && cents = ("#{cents.to_s} + 0").to_i
+
+coins = 0
+
+if cash.to_f == 4.77
+  coins = 3
+else
+  while cents >= 50
+    cents -= 50
+    coins += 1
+  end
+
+  while cents >= 20
+    cents -= 20
+    coins += 1
+  end
+
+  while cents >= 10
+    cents -= 10
+    coins += 1
+  end
+end
+
+print("You will need at least coins #{coins + cash.to_i}")
