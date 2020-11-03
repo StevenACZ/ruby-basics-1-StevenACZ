4f500f226f4c98cc549000ea78036f55773a23f6
stevenacz <scoailazaa@gmail.com>
2020-11-03 10:25:49 -0500

    Problema 05

--- 05cash/cash.rb
+++ 05cash/cash.rb
@@ -1,33 +1,19 @@
 # This exercise won't have useful comments to guide you. Go for it!
 # 1 Sol, 50 centimos, 20 centimos and 10 centimos
 
+def change_cash cash
+  sol, cent = cash.round(1).to_s.split('.')
+  five_cent, other_cent = cent.to_i.divmod(5)
+  twenty_cent, ten_cent = other_cent.divmod(2)
+  return [sol.to_i, five_cent, twenty_cent, ten_cent].sum
+end
+
 cash = 0
 loop do
   print("Change owed: ")
   cash = gets.to_f
-
+  
   cash.positive? && break
 end
 
-cents = cash.to_s.split(".").last.to_i
-cash.to_i.zero? && cents = cash.to_s.split(".").last.to_i.round(-1)
-cents.to_s.length == 1 && cents = "#{cents} + 0".to_i
-
-coins = 0
-
-while cents >= 50
-  cents -= 50
-  coins += 1
-end
-
-while cents >= 20
-  cents -= 20
-  coins += 1
-end
-
-while cents >= 10
-  cents -= 10
-  coins += 1
-end
-
-print("You will need at least coins #{coins + cash.to_i}")
+print("You will need at least coins #{change_cash(cash)}")
