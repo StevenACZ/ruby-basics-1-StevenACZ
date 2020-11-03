f5cef4a31f5fd6644e1c378f8a205e4bef8cd4c3
stevenacz <scoailazaa@gmail.com>
2020-11-03 10:26:12 -0500

    Problema 05

--- 05cash/cash.rb
+++ 05cash/cash.rb
@@ -1,18 +1,17 @@
 # This exercise won't have useful comments to guide you. Go for it!
 # 1 Sol, 50 centimos, 20 centimos and 10 centimos
 
-def change_cash cash
-  sol, cent = cash.round(1).to_s.split('.')
+def change_cash(cash)
+  sol, cent = cash.round(1).to_s.split(".")
   five_cent, other_cent = cent.to_i.divmod(5)
   twenty_cent, ten_cent = other_cent.divmod(2)
-  return [sol.to_i, five_cent, twenty_cent, ten_cent].sum
+  [sol.to_i, five_cent, twenty_cent, ten_cent].sum
 end
 
 cash = 0
 loop do
   print("Change owed: ")
   cash = gets.to_f
-  
   cash.positive? && break
 end
 
